74f99a795d36be5a16c917f356249f7b5993eb96
Steph Nugs <steph.bowie1993@gmail.com>
2023-09-07 15:35:58 -0600

    refactored getUserWithEmail, getUserWithId, and addUser functiuons

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -18,14 +18,21 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  const queryString = `
+    SELECT *
+    FROM users
+    WHERE email = $1;
+  `;
+
+  return pool.query(queryString, [email])
+    .then((result) => {
+      // Check if a user with the given email exists
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null; // User not found
+      }
+    })
 };
 
 /**
@@ -34,7 +41,20 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1;
+  `;
+
+return pool.query(queryString, [id])
+  .then((result) => {
+    if (result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      return null;
+    } 
+  })
 };
 
 /**
@@ -43,10 +63,18 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `;
+
+  const values = [user.name, user.email, user.password];
+
+  return pool.query(queryString, values)
+    .then((result) => {
+      return result.rows[0];
+    })
 };
 
 /// Reservations
